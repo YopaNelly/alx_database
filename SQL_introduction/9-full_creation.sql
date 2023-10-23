abd12a71285ada003798691b0e58a3c6c8ddb469
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2023-10-23 20:14:13 +0100

    Added values to table created

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,3 +1,3 @@
 -- code to create a table in the db and add multiples rows
 
-CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT) VALUE(1, 'John', 10),(2, 'Alex', 3), (4, 'George', 8);
