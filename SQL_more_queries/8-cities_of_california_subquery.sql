e04dd0ae4b47b281f30b79453bcc033af9531fe6
Yopa Nelly <yopatatchou@gmail.com>
2023-10-28 15:24:49 +0100

    adde file

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- code that list all the cities of California that can be found in the DATABASE
 
-SELECT name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY ID ASC;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY ID ASC;
